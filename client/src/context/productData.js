e02447b0687b511c2a0d81d8ba0c737b4dda2890
pranav134 <pranav.134.pg@gmail.com>
2019-07-01 22:17:28 +0530

    no message

--- client/src/context/productData.js
+++ client/src/context/productData.js
@@ -3,7 +3,7 @@ export const items = [
     sys: { id: 0},
     fields: {
       title: "Urdu Calligraphy done an a T shirt",
-      price: 200.00,
+      price: 200,
       company: "Kitabat",
       description:
         "Under our Kitabat initiative, we have expanded by coming different products with Urdu Calligraphy. The T-shirt is a union of artistic Urdu Calligraphy and Smooth Cottom apparel wear. On the front we have an Urdu Drawing in the shape of a key.",
@@ -16,12 +16,53 @@ export const items = [
     sys: { id: 1},
     fields: {
       title: "Aesthetic frame in Urdu calligraphy",
+      price: 750,
+      company: "Kitabat",
+      description:
+        "Arabic Calligraphy forms an integral part of this heritage.It was introduced in India around the 7th century by early Arabic traders.\
+        The core purpose of its existence is Spirituality and was initiated for preserving the scripts of the Holy Quran.The above artwork is made by one of our very own 'katibs', Mohd. Ghalib. It is an extract from the Surah-Sabr meaning. Be, then, patient. Surely, the good end is for the righteous.",
+      featured: true,
+      image: { fields: { file: { url: "img/kitabat_frame.jpg" } } },
+      freeShipping: true
+    }
+  },
+  {
+    sys: { id: 2},
+    fields: {
+      title: "Surah Ikhlas",
+      price: 200,
+      company: "Kitabat",
+      description:[
+        "This exquisite artwork is made by one of our very own katibs, Mazahir Zaidi. The transcript is as follows : \n Trascript Bismillah-hir-Rahman-nir-raheem Qul huwa Allahu ahadun Allahu alssamadu  Lam yalid walam yooladu  Walam yakun lahu kufuwan ahadun Translation [Oh Pophet(pbuh) Say]: He, Allah, is One. Allah is He on Whom all depend. He begets not, nor is He begotten. And none is like Him.",
+      ],
+      featured: true,
+      image: { fields: { file: { url: "img/Surah_Ikhlas.jpg" } } },
+      freeShipping: true
+    }
+  },
+  {
+    sys: { id: 3},
+    fields: {
+      title: "Aesthetic frame in Urdu calligraphy",
       price: 200,
       company: "Kitabat",
       description:
-        "One of the most premium calligraphy artworks designed by the renowned katibs of Old Delhi themselves. ",
+        "Arabic Calligraphy forms an integral part of this heritage. It was introduced in India around the 7th century by early Arabic traders. The core purpose of its existence is Spirituality and was initiated for preserving the scripts of the Holy Quran.The above artwork is made by one of our very own 'katibs', Mohd. Ghalib. It is an extract from the Surah-Sabr meaning. Be, then, patient. Surely, the good end is for the righteous. ",
       featured: true,
-      image: { fields: { file: { url: "img/kitabat_frame.jpg" } } },
+      image: { fields: { file: { url: "img/bg_frame.jpg" } } },
+      freeShipping: true
+    }
+  },
+  {
+    sys: { id: 4},
+    fields: {
+      title: "Aesthetic frame in Urdu calligraphy",
+      price: 200,
+      company: "Kitabat",
+      description:
+        "Arabic Calligraphy forms an integral part of this heritage. It was introduced in India around the 7th century by early Arabic traders. The core purpose of its existence is Spirituality and was initiated for preserving the scripts of the Holy Quran.The above artwork is made by one of our very own 'katibs', Mohd. Ghalib. It is an extract from the Surah-Sabr meaning. Be, then, patient. Surely, the good end is for the righteous. ",
+      featured: true,
+      image: { fields: { file: { url: "img/Hadith.jpg" } } },
       freeShipping: true
     }
   }
